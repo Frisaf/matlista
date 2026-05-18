a08fd23b4b89b51e104f80f17563be8151d5c8fc
Frisaf <frisaf07@gmail.com>
2026-05-18 08:55:56 +0200

    import cookie session to user.js

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ import express from "express"
 import prisma from "../config/db.js"
 import { body, param, validationResult} from "express-validator"
 import bcrypt from "bcrypt"
-import { nextTick } from "node:process"
+import session from "cookie-session"
 
 const router = express.Router()
 
